439c1afe1c42a57c43f0eb2e7352f60721752252
Elton Fernando Doehnert <eltonfd@gmail.com>
2020-12-02 09:30:46 -0300

    modificacao

--- Plugin/CreateUserInGermini.php
+++ Plugin/CreateUserInGermini.php
@@ -5,13 +5,16 @@ use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\UrlFactory;
 use Magento\Framework\Message\ManagerInterface;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Directory\Model\RegionFactory;
 
 class CreateUserInGermini
 {
+    protected $regionFactory;
     protected $timezone;
     protected $_curl;
     protected $scopeConfig;
     protected $resultRedirect;
+    protected $_countryFactory;
 
     /** @var \Magento\Framework\UrlInterface */
     protected $urlModel;
@@ -39,9 +42,12 @@ class CreateUserInGermini
         \Magento\Framework\HTTP\Client\Curl $curl,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         ResultFactory $result,
-        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $timezone
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $timezone,
+        RegionFactory $regionFactory,
+        \Magento\Directory\Model\CountryFactory $countryFactory
     )
     {
+        $this->_countryFactory = $countryFactory;
         $this->_curl = $curl;
         $this->urlModel = $urlFactory->create();
         $this->resultRedirectFactory = $redirectFactory;
@@ -49,6 +55,7 @@ class CreateUserInGermini
         $this->scopeConfig = $scopeConfig;
         $this->resultRedirect = $result;
         $this->timezone = $timezone;
+        $this->regionFactory = $regionFactory;
     }
 
     /**
@@ -63,6 +70,10 @@ class CreateUserInGermini
     )
     {
         /** @var \Magento\Framework\App\RequestInterface $request */
+
+        $url_base = $this->scopeConfig->getValue('acessos/general/kernel_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+
+
         $email = $subject->getRequest()->getParam('email');
         $firstname = $subject->getRequest()->getParam('firstname');
         $lastname = $subject->getRequest()->getParam('lastname');
@@ -72,13 +83,122 @@ class CreateUserInGermini
         $dob = $subject->getRequest()->getParam('dob');
         $gender = $subject->getRequest()->getParam('gender');
 
+        $location = $subject->getRequest()->getParam('street')[0];
+        $number = $subject->getRequest()->getParam('street')[1];
+        $district = $subject->getRequest()->getParam('street')[2];
+        $zipCode = $subject->getRequest()->getParam('postcode');
+
+        $regionId = $subject->getRequest()->getParam('region_id'); //499
+        $region = $this->regionFactory->create()->load($regionId);
+        $stateId = $region->getCode();
+        $countryId = $region->getCountryId();
+        $country = $this->_countryFactory->create()->loadByCode($countryId);
+        $countryName = $country->getName();
+        $cityId = $subject->getRequest()->getParam('city');
+        $phone2 = $subject->getRequest()->getParam('telephone');
+        $phone2 = preg_replace("/[^0-9]/", "",$phone2);
+
+        // Get countryId from Germini
+        $response = "";
+        $url = $url_base . '/api/Country';
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Accept: text/plain'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $resultado = json_decode($response);
+
+        foreach ($resultado as $res){
+            if ($res->name == $countryName){
+                $countryId = $res->id;
+            }
+        }
+
+        curl_close($curl);
+        ///////////////////////////////
+
+        // Get stateId from Germini
+        $response = "";
+        $url = $url_base . '/api/State?countryId=' . $countryId;
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Accept: text/plain'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $resultado = json_decode($response);
+
+        foreach ($resultado as $res){
+            if ($res->abbreviation == $stateId){
+                $stateId = $res->id;
+            }
+        }
+
+        curl_close($curl);
+        ///////////////////////////////
+
+        // Get cityId from Germini
+        $response = "";
+        $url = $url_base . '/api/City?stateId=' . $stateId;
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Accept: text/plain'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $resultado = json_decode($response);
+
+        foreach ($resultado as $res){
+            if ($res->name == $cityId){
+                $cityId = $res->id;
+            }
+        }
+
+        curl_close($curl);
+        ///////////////////////////////
+
+
         $gender = $gender === "2" ? 'f' : 'm';
         $dob = date("Y-m-d H:i:s", strtotime($dob));
-        // $dob = $this->timezone->date(new \DateTime($dob))->format('Y-m-d H:i:s');
 
         // Cria usuário no Germini
-        $url_base = $this->scopeConfig->getValue('acessos/general/kernel_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-
         $response = "";
         $url = $url_base . '/api/Consumer/Register';
         $params = [
@@ -88,7 +208,18 @@ class CreateUserInGermini
             "password" => $password,
             "confirmPassword" => $password_confirmation,
             "gender" => $gender,
-            "dateOfBirth" => $dob
+            "dateOfBirth" => $dob,
+            "phoneNumber2" => $phone2,
+            "address" => [
+                "addressType" => 1,
+                "location" => $location,
+                "district" => $district,
+                "number" => $number,
+                "zipCode" => $zipCode,
+                "stateId" => $stateId,
+                "cityId" => $cityId,
+                "countryId" => $countryId
+            ]
         ];
 
         $data_json = json_encode($params);
@@ -104,7 +235,7 @@ class CreateUserInGermini
         curl_close($ch);
         $resultado = json_decode($response);
 
-        if ($resultado->errors == ''){
+        if (empty($resultado->errors)){
             return $proceed();
         } else {
             foreach ($resultado->errors as $error){
@@ -117,22 +248,5 @@ class CreateUserInGermini
                 'customer/account/create'
             );
         }
-
-
-
-        // list($nick, $domain) = explode('@', $email, 2);
-        // if (in_array($domain, ['163.com', 'mail.ru'], true)) {
-
-        //     $this->messageManager->addErrorMessage(
-        //         'Registration is disabled for you domain'
-        //     );
-        //     $defaultUrl = $this->urlModel->getUrl('*/*/create', ['_secure' => true]);
-        //     /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
-        //     $resultRedirect = $this->resultRedirectFactory->create();
-
-        //     return $resultRedirect->setUrl($defaultUrl);
-
-        // }
-
     }
 }
