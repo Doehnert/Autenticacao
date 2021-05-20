bc60324c5db2fbdf42e64fa7ec2388db9cc0c491
Elton Doehnert <eltonfd@gmail.com>
2021-05-20 17:58:42 -0300

    Correcoes e lista de cidades

---
+++ Controller/Germini/Cities.php
@@ -0,0 +1,105 @@
+<?php
+namespace Vexpro\Autenticacao\Controller\Germini;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\App\Action\Context;
+use \Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Directory\Model\RegionFactory;
+
+class Cities extends Action
+{
+		protected $resultJsonFactory;
+        protected $scopeConfig;
+        protected $resultRedirectFactory;
+        protected $messageManager;
+        protected $regionFactory;
+
+		public function __construct(
+			 Context $context,
+			 JsonFactory $resultJsonFactory,
+             ScopeConfigInterface $scopeConfig,
+             RedirectFactory $redirectFactory,
+             ManagerInterface $messageManager,
+             RegionFactory $regionFactory
+		)
+		{
+			$this->resultJsonFactory = $resultJsonFactory;
+            $this->scopeConfig = $scopeConfig;
+            $this->resultRedirectFactory = $redirectFactory;
+            $this->messageManager = $messageManager;
+            $this->regionFactory = $regionFactory;
+			return parent::__construct($context);
+		}
+
+		public function execute()
+		{
+            $post = $this->getRequest()->getPostValue();
+            $region_id = $post['region_id'];
+            $region = $this->regionFactory->create()->load($region_id);
+            $region_code = $region->getCode();
+
+            $url_base = $this->scopeConfig->getValue('acessos/general/kernel_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+            $url = $url_base . '/api/State/?countryId=20b32dbd-8bda-4563-bcd5-0a7e827fc5e4';
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                    'Accept: text/plain'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+            $estados = json_decode($response);
+
+            $meu_estado = '';
+            foreach ($estados as $estado){
+                if ($estado->abbreviation == $region_code){
+                    $meu_estado = $estado;
+                    break;
+                }
+            }
+
+            $germini_region_id = $meu_estado->id;
+
+            $url = $url_base . '/api/City/?stateId=' . $germini_region_id;
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                    'Accept: text/plain'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+            $cidades = json_decode($response);
+
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+            /** @var \Magento\Framework\Controller\Result\Json $resultJson */
+            $resultJson = $this->resultJsonFactory->create();
+            $resultJson->setData($cidades);
+
+            return $resultJson;
+		}
+}
