f741deea085281ed1cff200fcd5bfdef984e894b
Elton Doehnert <eltonfd@gmail.com>
2021-05-21 16:17:28 -0300

    Novas mudancas layout

--- Controller/Germini/Regulamentos.php
+++ Controller/Germini/Regulamentos.php
@@ -9,7 +9,7 @@ use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\Message\ManagerInterface;
 use Magento\Directory\Model\RegionFactory;
 
-class Cities extends Action
+class Regulamentos extends Action
 {
 		protected $resultJsonFactory;
         protected $scopeConfig;
@@ -36,13 +36,8 @@ class Cities extends Action
 
 		public function execute()
 		{
-            $post = $this->getRequest()->getPostValue();
-            $region_id = $post['region_id'];
-            $region = $this->regionFactory->create()->load($region_id);
-            $region_code = $region->getCode();
-
             $url_base = $this->scopeConfig->getValue('acessos/general/kernel_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-            $url = $url_base . '/api/State/?countryId=20b32dbd-8bda-4563-bcd5-0a7e827fc5e4';
+            $url = $url_base . '/api/ProgramRules/Regulation';
 
             $curl = curl_init();
 
@@ -61,45 +56,13 @@ class Cities extends Action
             ));
 
             $response = curl_exec($curl);
-            $estados = json_decode($response);
-
-            $meu_estado = '';
-            foreach ($estados as $estado){
-
-                if ($estado->abbreviation == $region_code){
-                    $meu_estado = $estado;
-                    break;
-                }
-            }
-
-            $germini_region_id = $meu_estado->id;
-
-            $url = $url_base . '/api/City/?stateId=' . $germini_region_id;
-
-            $curl = curl_init();
-
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $url,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'GET',
-                CURLOPT_HTTPHEADER => array(
-                    'Accept: text/plain'
-                ),
-            ));
-
-            $response = curl_exec($curl);
-            $cidades = json_decode($response);
+            $regras = json_decode($response);
 
             $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 
             /** @var \Magento\Framework\Controller\Result\Json $resultJson */
             $resultJson = $this->resultJsonFactory->create();
-            $resultJson->setData($cidades);
+            $resultJson->setData($regras);
 
             return $resultJson;
 		}
