e62e2747bd68c58991c7360f8931a45cb8eccdb6
Elton Doehnert <eltonfd@gmail.com>
2021-08-04 12:21:11 -0300

    Politica de privacidade hom

--- Controller/Germini/Policy.php
+++ Controller/Germini/Policy.php
@@ -10,7 +10,7 @@ use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\Message\ManagerInterface;
 use Magento\Directory\Model\RegionFactory;
 
-class Regras extends Action
+class Policy extends Action
 {
     protected $resultJsonFactory;
     protected $scopeConfig;
@@ -37,7 +37,7 @@ class Regras extends Action
     public function execute()
     {
         $url_base = $this->scopeConfig->getValue('acessos/general/kernel_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        $url = $url_base . '/api/PlatformRules';
+        $url = $url_base . '/api/ProgramRules/Regulation';
 
         $curl = curl_init();
 
@@ -55,8 +55,10 @@ class Regras extends Action
             ),
         ));
 
-        $response = curl_exec($curl);
-        $regras = json_decode($response);
+        // $response = curl_exec($curl);
+        // $regras = json_decode($response);
+        $regras = new \stdClass();
+        $regras->policy = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 
