c313d2a22634f20f1c169f67f974d00c9c842921
Elton Doehnert <eltonfd@gmail.com>
2021-09-17 15:02:10 -0300

    Correcao hom

--- Observer/EditCustomer.php
+++ Observer/EditCustomer.php
@@ -121,9 +121,9 @@ class EditCustomer implements \Magento\Framework\Event\ObserverInterface
 
                     // $generoMaiusculo = $genero == 1 ? "M" : "F";
 
-                    if ($cpf_apenas_numeros == "") {
-                        return;
-                    }
+                    // if ($cpf_apenas_numeros == "") {
+                    //     return;
+                    // }
 
                     $xmlstr = "<?xml version='1.0' standalone='yes'?>
                     <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:cvale:i17:014\">
