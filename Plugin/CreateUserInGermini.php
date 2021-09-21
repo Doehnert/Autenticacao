56050a467bd149cb4f2b29927a652cfde930f8dc
Elton Doehnert <eltonfd@gmail.com>
2021-09-21 12:04:18 -0300

    Correcao hom

--- Plugin/CreateUserInGermini.php
+++ Plugin/CreateUserInGermini.php
@@ -355,9 +355,7 @@ class CreateUserInGermini
             // curl_setopt($ch, CURLOPT_TIMEOUT_MS, 1);
             // curl_setopt($ch, CURLOPT_NOSIGNAL, 1);
             curl_setopt($ch, CURLOPT_TIMEOUT, 4000);
-
-
-            // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3000);
             curl_setopt($ch, CURLOPT_HTTPHEADER, [
                 "Authorization: Basic cGlfZ2VybWluaTpjdmFsZTIwMTQ=",
                 "Content-Type: text/xml",
@@ -365,6 +363,11 @@ class CreateUserInGermini
             curl_setopt($ch, CURLOPT_POST, 1);
 
             $data = curl_exec($ch);
+
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            if ($httpCode == 504) {
+                $logger->info("Erro 504");
+            }
             curl_close($ch);
 
             //convert the XML result into array
